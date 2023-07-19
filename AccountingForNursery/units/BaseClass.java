0caf13f7ecf37e5768c4b2152dfe685d3f96478b
Python <Pythonchic@gmail.com>
2023-07-19 19:04:21 +0300

    описал коструктор BaseClass

--- AccountingForNursery/units/BaseClass.java
+++ AccountingForNursery/units/BaseClass.java
@@ -1,4 +1,17 @@
 package AccountingForNursery.units;
 
-public class BaseClass {
+import java.util.ArrayList;
+
+public abstract class BaseClass implements Interface {
+    protected String name;
+    protected ArrayList<String> commands;
+    protected int age;
+
+    protected BaseClass(String name, ArrayList<String> commands, int age) {
+        this.name = name;
+        this.commands = commands;
+        this.age = age;
+    }
+
+
 }
